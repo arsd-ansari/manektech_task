4cc3862a747e92380b5c0f68ff4e288113a373fb
arshad-dqot <arshad@dqotsolutions.com>
2023-04-12 12:40:55 +0530

    manektech task done

---
+++ lib/data/modal/cart_modal.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+
+import 'package:manektech_task/data/modal/product_modal.dart';
+
+class CartModal {
+  int cartId;
+  ProductModal product;
+  int qty;
+
+  CartModal(this.cartId, this.product, this.qty);
+
+  factory CartModal.fromDb(Map data) {
+    return CartModal(data['id'],
+        ProductModal.fromJson(json.decode(data['product'])), data['qty']);
+  }
+}
